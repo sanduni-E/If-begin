1a9f7576a002c93fe2987a985ad07bc196c808c1
sanduni-E <sanduniue@gmail.com>
2019-02-17 21:40:19 +0530

    If-begin
    If-begin

---
+++ src/pkgif/begin/IfBegin.java
@@ -0,0 +1,21 @@
+
+package pkgif.begin;
+
+
+public class IfBegin {
+
+   
+    public static void main(String[] args) {
+       int num=5;
+       if(num>5){
+           System.out.println("number is greater than 5");
+       }
+       if(num<5){
+           System.out.println("number is less than 5");
+       }
+       if(num==5){
+           System.out.println("number is equal to 5");
+       }
+    }
+    
+}
